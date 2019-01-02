73dce82f4a739a9f7d50619cf0afc1a9ea279b6e
Trent Moss <trentmoss05@gmail.com>
2019-01-02 15:58:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,11 @@ def summon_captain_planet(array)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.any? do |calls|
+    calls.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
