9225da1afd0e89193a8f757e61a6a1c4bda61d5c
Trent Moss <trentmoss05@gmail.com>
2019-01-02 15:52:21 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,12 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect do |word|
+    word.first.upcase
+
 end
 
 def long_planeteer_calls# code an argument here
