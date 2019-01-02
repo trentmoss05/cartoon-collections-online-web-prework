5e6ed73a79be010bca6c3834fc8d39c841ac775f
Trent Moss <trentmoss05@gmail.com>
2019-01-02 15:55:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,9 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-
-
+  array.map do |call|
+    call.capitalize + '!'
+  end
 end
 
 def long_planeteer_calls# code an argument here
